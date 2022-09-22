d4edd08f06a6ba2d33a84e6963b4c5944a1fc78f
hhmadan <hhmadan03@gmail.com>
2022-09-22 21:30:39 +0530

    Added: UC3_DeterminePercentageOfDoubletCombination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,46 @@
 echo "Enter the number of Flips: " 
 read no_of_flips
-declare -A singlet_frequency=( 
 
-                            [H]=0
-                            [T]=0
+declare -A doublet_frequency=( 
+
+                                [HH]=0
+                                [HT]=0
+                                [TH]=0
+                                [TT]=0
 
                               )
+
 for(( flip = 1; flip <= no_of_flips; flip++ ))
 do
     echo -n "flip $flip "
-    (( toss = RANDOM % 2 ))
-    case $toss in 
-        0)
-            echo "Heads"
-            (( singlet_frequency[H]++ ))
+    (( coin_1 = RANDOM % 2 ))
+    (( coin_2 = RANDOM % 2 ))
+
+    #0 is mapped as heads
+    #1 is mapped as tails
+    case $coin_1$coin_2 in 
+        00)
+            echo "Heads Heads"
+            (( doublet_frequency[HH]++ )) 
             ;;
-        1)
-            echo "Tails"
-            (( singlet_frequency[T]++ ))
+        01)
+            echo "Heads Tails"
+            (( doublet_frequency[HT]++ ))
+            ;;
+        10)
+            echo "Tails Heads"
+            (( doublet_frequency[TH]++ ))
+            ;;
+        11)
+            echo "Tails Tails"
+            (( doublet_frequency[TT]++ ))
+            ;;
     esac
 done
 
-for combination in ${!singlet_frequency[@]}
+for combination in ${!doublet_frequency[@]}
 do
-    percentage=$(( ${singlet_frequency[$combination]}  * 100 / no_of_flips ))
-    singlet_frequency[$combination]=$percentage
-    echo "Percentage of $combination is ${singlet_frequency[$combination]}%"   
+    percentage=$(( ${doublet_frequency[$combination]}  * 100 / no_of_flips ))
+    doublet_frequency[$combination]=$percentage
+    echo "percentage of $combination is ${doublet_frequency[$combination]}%"   
 done
